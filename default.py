35bc73f2f255251f85623a89675fe7c5ed38e6fb
Tobias D. Oestreicher <oestreicher@b1-systems.de>
2018-01-26 22:11:38 +0100

    Added function for read fhem values

--- default.py
+++ default.py
@@ -49,40 +49,6 @@ from resources.lib.PH import PluginHelpers
 ##########################################################################################################################
 ##
 ##########################################################################################################################
-def show_bulispielplan(liga):
-    DETAILWIN = xbmcgui.WindowXMLDialog('bulilist-spielplan-DialogWindow.xml', __addonDir__, 'Default', '720p')
-    DETAILWIN.doModal()
-
-##########################################################################################################################
-##
-##########################################################################################################################
-def show_bulinaechsterspieltag(liga):
-    DETAILWIN = xbmcgui.WindowXMLDialog('bulilist-naechsterspieltag-DialogWindow.xml', __addonDir__, 'Default', '720p')
-    DETAILWIN.doModal()
-
-##########################################################################################################################
-##
-##########################################################################################################################
-def show_bulilist(liga):
-    DETAILWIN = xbmcgui.WindowXMLDialog('bulilist-platzierung-DialogWindow.xml', __addonDir__, 'Default', '720p')
-    DETAILWIN.doModal()
-
-##########################################################################################################################
-##
-##########################################################################################################################
-def show_unwetterwarnungen():
-    DETAILWIN = xbmcgui.WindowXMLDialog('unwetterwarnungen-DialogWindow.xml', __addonDir__, 'Default', '720p')
-    DETAILWIN.doModal()
-
-
-
-
-
-# To Remove: docu.cc Widget
-
-##########################################################################################################################
-##
-##########################################################################################################################
 def getUnicodePage(url):
     req = urllib2.urlopen(url)
     content = ""
@@ -167,12 +133,9 @@ else:
 if file != '':
     WINDOW.setProperty( "FHEMControl.Window", file )
     WINDOW.setProperty( "FHEMControl.Title", title )
-    #xbmc.executebuiltin('Skin.SetString("fhemjsonfile","%s" % (file))')
     ph.writeLog('SETFILE:         %s' % (file), level=xbmc.LOGDEBUG)
 
 ph.writeLog('FILE:         %s' % (file), level=xbmc.LOGDEBUG)
-
-
 ph.writeLog('IP  :         %s' % (ip), level=xbmc.LOGDEBUG)
 ph.writeLog('PORT:         %s' % (port), level=xbmc.LOGDEBUG)
 ph.writeLog('CMD :         %s' % (cmd), level=xbmc.LOGDEBUG)
@@ -190,18 +153,10 @@ elif methode=='stop_service':
 # Selects
 elif methode=='get_sets_items':
 
-    ph.writeLog("IN get_sets_items:         %s" % (xbmc.getInfoLabel('Skin.String(fhemjsonfile)')), level=xbmc.LOGDEBUG)
-    ph.writeLog("IN get_sets_items:         %s" % (title), level=xbmc.LOGDEBUG)
     file=WINDOW.getProperty("FHEMControl.Window")
     title=WINDOW.getProperty("FHEMControl.Title")
     ph.writeLog("IN get_sets_items2:         %s" % (title), level=xbmc.LOGDEBUG)
-    #xbmc.executebuiltin("Skin.SetString(fhemjsonfile,file,4711815)")
-#    if file != '':
     feedjson = fhemdialogs.feed2container2(file,title)
-#      xbmc.executebuiltin("Skin.SetString(fhemjsonfile,file)")
-#    else:
-#      feedjson = fhemdialogs.feed2container2("fhemsetheizung.json")
-#      xbmc.executebuiltin('Skin.SetString(fhemjsonfile,"fhemsetheizung.json")')
 
     for sitem in feedjson:
         li = xbmcgui.ListItem(sitem['Label'], iconImage=sitem['PIC'])
@@ -213,249 +168,9 @@ elif methode=='get_sets_items':
     xbmcplugin.endOfDirectory(addon_handle)
 
 elif methode=='show_sets_window':
-    #xbmc.executebuiltin("Skin.SetString(fhemjsonfile,file,4711815)")
     DETAILWIN = xbmcgui.WindowXMLDialog('sets-DialogWindow.xml', __addonDir__, 'Default', '720p')
     DETAILWIN.doModal()
 
 
-elif methode=='show_livestream_select_dialog':
-    dialog = xbmcgui.Dialog()
-    ret = dialog.select(__LS__(30190), [__LS__(30191), __LS__(30192), __LS__(30193), __LS__(30194), __LS__(30195), __LS__(30196)])
-    if ret == 0:
-        nls.PlayTagesschau24()
-    elif ret == 1:
-        nls.PlayEuronews()
-    elif ret == 2:
-        nls.PlayNTV()
-    elif ret == 3:
-        nls.PlayN24()
-    elif ret == 4:
-        nls.PlayPhoenix()
-    elif ret == 5:
-        nls.PlayDW()
 
-elif methode=='show_buli_select':
-    dialog = xbmcgui.Dialog()
-    ret = dialog.select(str(__LS__(30153)), [str(__LS__(30128)), str(__LS__(30129)), str(__LS__(30125)), str(__LS__(30126)), __LS__(30091), __LS__(30092)])
-    if ret == 0:
-        WINDOW.setProperty("NewsCenter.Buli.LigaInfo", "1" )
-        show_bulilist(1)        
-    elif ret == 1:
-        WINDOW.setProperty("NewsCenter.Buli.LigaInfo", "2" )
-        show_bulilist(2)
-    elif ret == 2:
-        WINDOW.setProperty("NewsCenter.Buli.LigaInfo", "1" )
-        show_bulispielplan("1")
-    elif ret == 3:
-        WINDOW.setProperty("NewsCenter.Buli.LigaInfo", "2" )
-        show_bulispielplan(2)
-    elif ret == 4:
-        WINDOW.setProperty("NewsCenter.Buli.LigaInfo", "1" )
-        show_bulinaechsterspieltag(1)
-    elif ret == 5:
-        WINDOW.setProperty("NewsCenter.Buli.LigaInfo", "2" )
-        show_bulinaechsterspieltag(2)
-
-
-
-
-# Settings
-elif methode=='set_default_feed':
-    allfeeds = json.loads(str(ConfigFeeds))
-    feedname=[]
-    for f in allfeeds:
-        feedname.append(f['name'])
-
-    dialog = xbmcgui.Dialog()
-    ret = dialog.select(str(__LS__(30155)),feedname)
-    defaultfeedname=allfeeds[ret]['name']
-    __addon__.setSetting('storedefault',defaultfeedname)
-
-
-
-
-# Dialogwindows
-elif methode=='show_bulilist':
-    WINDOW.setProperty("NewsCenter.Buli.LigaInfo", buliliga )
-    show_bulilist(buliliga)
-
-elif methode=='show_bulispielplan':
-    WINDOW.setProperty("NewsCenter.Buli.LigaInfo", buliliga )
-    show_bulispielplan(buliliga)
-
-elif methode=='show_bulinaechsterspieltag':
-    WINDOW.setProperty("NewsCenter.Buli.LigaInfo", buliliga )
-    show_bulinaechsterspieltag(buliliga)
-
-elif methode=='show_wetter_karte_bundesland':
-    DETAILWIN = xbmcgui.WindowXMLDialog('wetterkarten-bundesland-DialogWindow.xml', __addonDir__, 'Default', '720p')
-    DETAILWIN.doModal()
-
-
-
-
-# Container
-elif methode=='get_buli_spielplan_items':
-    spielelist = nb.get_buli_spielplan_items(buliliga)
-    url = '-'
-    for sitem in spielelist:
-        li = xbmcgui.ListItem(sitem['Label'], iconImage=sitem['Logo'])
-        li.setProperty("Spieldatum", str(sitem['Spieldatum']))
-        li.setProperty("Mannschaft1", sitem['Mannschaft1'])
-        li.setProperty("Mannschaft2", sitem['Mannschaft2'])
-        li.setProperty("Mannschaft1Logo", sitem['Mannschaft1Logo'])
-        li.setProperty("Mannschaft2Logo", sitem['Mannschaft2Logo'])
-        li.setProperty("Ergebniss", str(sitem['Ergebniss']))
-        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
-    xbmcplugin.endOfDirectory(addon_handle)
-
-
-elif methode=='get_buli_table_items':
-    bulilist = nb.get_buli_table_items(buliliga)
-    url = '-'
-    for sitem in bulilist:
-        Logo = sitem['Logo']
-        Logo = Logo.encode('utf-8')
-        li = xbmcgui.ListItem(sitem['Label'], iconImage=Logo)
-        li.setProperty("Spiele", str(sitem['Spiele']))
-        li.setProperty("SUN", sitem['SUN'])
-        li.setProperty("Platz", str(sitem['Platz']))
-        li.setProperty("Tore", str(sitem['Tore']))
-        li.setProperty("Punkte", str(sitem['Punkte']))
-        li.setProperty("StatPic", str(sitem['StatPic']))
-        li.setProperty("Logo", str(sitem['Logo']))
-        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
-    xbmcplugin.endOfDirectory(addon_handle)
-
-
-elif methode=='get_buli_naechsterspieltag_items':
-    bulilist = nb.get_buli_naechsterspieltag_items(buliliga)
-    url = '-'
-    for sitem in bulilist:
-        li = xbmcgui.ListItem(sitem['Label'], iconImage=__icon__)
-        li.setProperty("Mannschaft1", sitem['Mannschaft1'])
-        li.setProperty("Mannschaft2", sitem['Mannschaft2'])
-        li.setProperty("Mannschaft1Logo", str(sitem['Logo1']))
-        li.setProperty("Mannschaft2Logo", str(sitem['Logo2']))
-        li.setProperty("Spieldatum", str(sitem['Label']))
-        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
-    xbmcplugin.endOfDirectory(addon_handle)
-
-
-
-elif methode=='get_feed_items':
-    feedjson = nf.feed2container2()
-    for sitem in feedjson:
-        li = xbmcgui.ListItem(sitem['Label'], iconImage=sitem['Logo'])
-        li.setProperty("Desc", sitem['Desc'])
-        li.setProperty("HeaderPic", sitem['HeaderPic'])
-        li.setProperty("Date", sitem['Date'])
-        xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
-    xbmcplugin.endOfDirectory(addon_handle)
-
-
-
-
-# Wetterkarten
-elif methode=='get_dwd_pics_base':
-        nwk.DWD_Base(clickable)
-
-elif methode=='get_dwd_pics_base_uwz':
-        nwk.DWD_Base_UWZ(clickable)
-
-elif methode=='get_dwd_pics_extended':
-        nwk.DWD_Extended(clickable)
-
-elif methode=='get_dwd_pics_bundesland':
-        nwk.DWD_Bundesland(clickable)
-
-elif methode=='get_dwd_pics_bundesland_uwz':
-        nwk.DWD_Bundesland_UWZ(clickable)
-
-elif methode=='get_dwd_pics_base_extended':
-        nwk.DWD_Base_Extended(clickable)
-
-elif methode=='get_euronews_wetter_pics':
-        nwk.Euronews(clickable)
-
-
-
-
-# Pollen
-elif methode=='get_pollen_items':
-        npf = NewsPollenflug()
-        npf.get_pollen_items()
-
-
-
-
-# Unwetter
-elif methode=='get_unwetter_warnungen':
-        if __addon__.getSetting('plz') != '':
-            nuwz.Warnungen("DE", __addon__.getSetting('plz'))
-        else:
-            notifyheader= str(__LS__(30010))
-            xbmc.executebuiltin('XBMC.Notification('+notifyheader+', '+str(__LS__(30149))+' ,4000,'+__icon__+')')
-
-elif methode=='get_uwz_count':
-        if __addon__.getSetting('plz') != '':
-            nuwz.WarnAnzahl("DE", __addon__.getSetting('plz'))
-        else:
-            notifyheader=str(__LS__(30010))
-            xbmc.executebuiltin('XBMC.Notification('+notifyheader+', '+str(__LS__(30149))+' ,4000,'+__icon__+')')
-
-elif methode=='get_uwz_maps':
-        nwk.UWZ(clickable)
-
-elif methode=='show_unwetter_warnungen':
-	show_unwetterwarnungen()
-
-
-
-
-# Default Entry
-elif methode=='refresh':
-        WINDOW.setProperty( "LatestNews.Service", "active" )
-        notifyheader= str(__LS__(30010))
-        notifytxt   = str(__LS__(30108))
-        xbmc.executebuiltin('XBMC.Notification('+notifyheader+', '+notifytxt+' ,4000,'+__icon__+')')
-        allfeeds = json.loads(str(ConfigFeeds))
-        storedefault=__addon__.getSetting('storedefault')
-        if storedefault != '':
-            for f in allfeeds:
-                if f['name'] == storedefault:
-                    url=f['url']
-                    pic=f['pic']
-                    break
-        else:
-            url="http://www.kodinerds.net/index.php/BoardFeed/?at=30575-8e710f12c83d6c7f66184ca3354f2c83baf4bbed"
-            pic="http://www.kodinerds.net/images/wbbLogo.png"
-        nf.feed2property(url, pic)
-
-        if __addon__.getSetting('plz') != '':
-            nuwz.WarnAnzahl("DE", __addon__.getSetting('plz'))
-            WINDOW.setProperty( "NewsCenter.PLZ", __addon__.getSetting('plz') )
-            WINDOW.setProperty( "NewsCenter.Bundesland", __addon__.getSetting('storebundesland') )
-            WINDOW.setProperty( "NewsCenter.Ort", __addon__.getSetting('storeort') )
-        else:
-            xbmc.executebuiltin('XBMC.Notification('+notifyheader+', '+str(__LS__(30149))+' ,4000,'+__icon__+')')
-        # To Remove: LatestDokus Widget
-        #set_LatestDokus_to_Home('http://doku5.com/api.php?search=UFO&page=1&noDYV=on')
-
-
-
-
-elif methode==None:
-
-    allfeeds = json.loads(str(ConfigFeeds))
-    feedname=[]
-    for f in allfeeds:
-        feedname.append(f['name'])
-    dialog = xbmcgui.Dialog()
-    ret = dialog.select("News Auswahl",feedname)
-    headerpic=allfeeds[ret]['pic']
-    url=allfeeds[ret]['url']
-    notifyheader= str(__LS__(30010))
-    xbmc.executebuiltin('XBMC.Notification('+notifyheader+', '+str(__LS__(30150))+' ,8000,'+__icon__+')')
-    nf.feed2property(url, headerpic)
 
---
+++ service.py
@@ -0,0 +1,192 @@
+'''
+    FHEM-Control for XBMC
+    Copyright (C) 2017 TDOe
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+import xbmc
+import xbmcaddon
+import xbmcgui
+import time
+import os
+import sys
+import json
+import re
+import telnetlib
+
+__settings__   = xbmcaddon.Addon(id='plugin.program.fhemcontrol')
+__cwd__        = __settings__.getAddonInfo('path')
+__icon__       = os.path.join(__cwd__,"icon.png")
+__scriptname__ = "FHEM-Control"
+
+__addon__     = xbmcaddon.Addon()
+__addonID__   = __addon__.getAddonInfo('id')
+__addonname__ = __addon__.getAddonInfo('name')
+__addonUserDataFolder__ = xbmc.translatePath("special://profile/addon_data/"+__addonID__).decode('utf-8')
+__version__   = __addon__.getAddonInfo('version')
+__path__      = __addon__.getAddonInfo('path')
+__LS__        = __addon__.getLocalizedString
+__icon__      = xbmc.translatePath(os.path.join(__path__, 'icon.png'))
+
+
+
+
+BASE_RESOURCE_PATH = xbmc.translatePath( os.path.join( __cwd__, 'resources', 'lib' ) )
+sys.path.append (BASE_RESOURCE_PATH)
+
+from settings import *
+#from fhem import *
+
+global g_failedConnectionNotified
+
+def writeLog(message, level=xbmc.LOGNOTICE):
+        xbmc.log('[%s %s]: %s' % (__addonID__, __version__,  message.encode('utf-8')), level)
+
+
+
+
+
+def initGlobals():
+  global g_failedConnectionNotified
+
+  g_failedConnectionNotified = False   
+  settings_initGlobals()
+
+def process_fhem():
+  while not xbmc.abortRequested:
+    fetchFhem()
+    for i in range(1,30):
+      time.sleep(1)
+      if xbmc.abortRequested:
+        break
+
+
+def fhem_get_json(ip,port,fhemdev):
+  ret = ""
+  try:
+    tn = telnetlib.Telnet()
+    writeLog("Connect to fhem %s:%s" % (str(ip),str(port)), level=xbmc.LOGNOTICE)
+    tn.open(ip, port)
+    tn.write("jsonlist2 %s\n" % (str(fhemdev)))
+    tn.write("quit\n")
+    ret = tn.read_all()
+    tn.close()
+    ret = re.sub(r'Bye...', '',str(ret))
+  except:
+    writeLog("telnet exception, could not connect to fhem instance",  level=xbmc.LOGNOTICE)
+  return ret
+
+
+
+def kodi_set_infovar(key,value):
+    writeLog('Setting INFO Var', level=xbmc.LOGNOTICE)
+    xbmcgui.Window(10000).setProperty('%s' % (key), value)
+
+
+def fhem_process(ip,port,DeviceList,AllReadings,AllInternals):
+    FHEMJsonRes=json.loads(fhem_get_json(ip,port,DeviceList))
+    
+    for Entity in FHEMJsonRes['Results']:
+        for Reading in AllReadings:
+            if Reading['FHEMDev'] == Entity['Name']:
+                SearchKey=Reading['SearchKey']
+                INFOVar=Reading['INFOVar']
+                logtext="INFOVar %s Entity %s[%s] -> %s" % (Reading['INFOVar'],Reading['FHEMDev'],Reading['SearchKey'],Entity['Readings'][Reading['SearchKey']]['Value'])
+                writeLog(logtext,level=xbmc.LOGNOTICE)
+                kodi_set_infovar(Reading['INFOVar'],Entity['Readings'][Reading['SearchKey']]['Value'])
+                break
+    
+        for Internal in AllInternals:
+            if Internal['FHEMDev'] == Entity['Name']:
+                SearchKey=Internal['SearchKey']
+                INFOVar=Internal['INFOVar']
+                logtext="INFOVar %s Entity %s[%s] -> %s" % (Internal['INFOVar'],Internal['FHEMDev'],Internal['SearchKey'],Entity['Internals'][Internal['SearchKey']])
+                writeLog(logtext,level=xbmc.LOGNOTICE)
+                kodi_set_infovar(Internal['INFOVar'],Entity['Internals'][Internal['SearchKey']])
+                break
+    
+
+
+
+
+
+def fetchFhem():
+  global g_failedConnectionNotified
+  
+  hostip   = settings_getHostIp()
+  hostport = settings_getHostPort()
+  
+  while not xbmc.abortRequested:
+    #check for new settings
+    if settings_checkForNewSettings():    #networksettings changed?
+      g_failedConnectionNotified = False  #reset notification flag
+    hostip   = settings_getHostIp()
+    hostport = settings_getHostPort()    
+    writeLog("hostip=%s" % (hostip),level=xbmc.LOGNOTICE)
+    writeLog("hostport=%s" % (hostport),level=xbmc.LOGNOTICE)
+    writeLog("DeviceList=%s" % (DeviceList),level=xbmc.LOGNOTICE)
+    FHEMJsonRes=json.loads(fhem_get_json(hostip,hostport,DeviceList))
+    if not FHEMJsonRes:
+      writeLog('Connection Failed', level=xbmc.LOGNOTICE)
+      #print "fhem: connection to fhem failed"
+      count = 10
+      while (not xbmc.abortRequested) and (count > 0):
+        time.sleep(1)
+        count -= 1
+      if not g_failedConnectionNotified:
+        g_failedConnectionNotified = True
+        text = __settings__.getLocalizedString(500)
+        xbmc.executebuiltin("XBMC.Notification(%s,%s,%s,%s)" % (__scriptname__,text,10,__icon__))
+    else:
+      text = __settings__.getLocalizedString(501)
+      if not g_failedConnectionNotified:
+        xbmc.executebuiltin("XBMC.Notification(%s,%s,%s,%s)" % (__scriptname__,text,10,__icon__))
+        g_failedConnectionNotified = True
+      fhem_process(hostip,hostport,DeviceList,AllReadings,AllInternals)  
+      writeLog('Connection Successfull', level=xbmc.LOGNOTICE)
+      break
+  return True
+
+#MAIN - entry point
+initGlobals()
+
+FHEMGetFile = '%s/fhemget.json' % (__addonUserDataFolder__) 
+try:
+    with open(FHEMGetFile, 'r') as toread:
+            ConfigReads=toread.read().rstrip('\n')
+except:
+    writeLog('Could not open configfile fhemget.json', level=xbmc.LOGNOTICE)
+
+allreads = json.loads(str(ConfigReads))
+
+
+AllReadings=allreads['Readings']
+AllInternals=allreads['Internal']
+Keys=[]
+for Reading in AllReadings:
+    Keys.append(Reading['FHEMDev'])
+
+for Internal in AllInternals:
+    Keys.append(Internal['FHEMDev'])
+
+DeviceList=','.join(Keys)
+
+
+
+
+#main loop
+while not xbmc.abortRequested:
+  settings_setup()
+  process_fhem()    #fhem loop
+
